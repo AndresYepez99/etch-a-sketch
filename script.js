8149405a36842be1f6f5d16a837e66bb79d5859e
Andres Yepez <andresyepezmn@gmail.com>
2023-01-19 15:21:12 -0500

    Agregar secuencia de comandos

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 961; i++) {
+    const colorPiece = document.createElement('div');
+    container.append(colorPiece);
+}
