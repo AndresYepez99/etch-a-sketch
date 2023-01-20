a04552263855e4532df6d10edb220da9a673f509
Andres Yepez <andresyepezmn@gmail.com>
2023-01-20 12:56:30 -0500

    Agregar funcion para borrar celdas

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-const container = document.querySelector('.container');
+const board = document.querySelector('.container-board'); //Tablero
 
-for (let i = 0; i < 961; i++) {
-    const colorPiece = document.createElement('div');
-    container.append(colorPiece);
+
+/*btn list*/ 
+const pixelIndicator = document.querySelector('.pixel-indicator');
+const btnCellSize = document.querySelector('#btn-range');
+
+const DEFAULT_SIZE = btnCellSize.value = 16;
+pixelIndicator.textContent = DEFAULT_SIZE;
+
+boardDimension(DEFAULT_SIZE);
+addCells(DEFAULT_SIZE);
+
+
+//Crea las filas y columnas que tendra el tablero 
+function boardDimension (size) {
+    board.style.cssText = `grid-template-columns: repeat(${size}, 1fr); 
+                           grid-template-rows: repeat(${size}, 1fr)`;
 }
+
+
+function addCells (size) {
+    for (let i = 0; i < size * size; i++) {
+        let newDiv = document.createElement('div');
+        board.appendChild(newDiv);
+    }
+}
+
+
+btnCellSize.addEventListener ('input', (event) => {
+    let size = event.target.value;
+    pixelIndicator.textContent = size;
+    boardDimension(size);
+    addCells(size);
+});
+
+//boardDimension(3);
+//addCells(3);
+
+
+
